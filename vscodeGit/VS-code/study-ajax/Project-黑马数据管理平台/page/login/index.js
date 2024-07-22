37d6f7fab2b5790abe25325d85f3c79c28198599
Oy-Jiang <Oy_jrb@163.com>
2024-07-22 10:46:51 +0800

    ajax小项目完成

---
+++ vscodeGit/VS-code/study-ajax/Project-黑马数据管理平台/page/login/index.js
@@ -0,0 +1,26 @@
+/**
+ * 目标1：验证码登录
+ * 1.1 在 utils/request.js 配置 axios 请求基地址
+ * 1.2 收集手机号和验证码数据
+ * 1.3 基于 axios 调用验证码登录接口
+ * 1.4 使用 Bootstrap 的 Alert 警告框反馈结果给用户
+ */
+const loginForm=document.querySelector('.login-form')
+document.querySelector('.btn-primary').addEventListener('click',()=>{
+    const data=serialize(loginForm,{hash:true,empty:true})
+    // console.log(data);
+    axios({
+        url:'/v1_0/authorizations',
+        method:'post',
+        data
+    }).then(res=>{
+        myAlert(true,'登陆成功')
+        console.log(res);
+    }).catch(err=>{
+        myAlert(false,err.response.data.message)
+        console.log(err);
+
+        // alert(err.response.data.message)
+        // document.querySelector('.code ').value=''
+    })
+})
